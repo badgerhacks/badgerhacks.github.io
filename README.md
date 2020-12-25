20d93adbbfd5f7a76c196dcf13db933c30d777f2
rdeepak2002 <rdeepak2002@gmail.com>
2020-12-25 01:08:23 -0800

    added particles.js

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Requirements
 
-Git must be installed
+Git must be installed.
 
 ## Available Scripts
 
 In the project directory, you can run:
 
-### `npm run start`
+### `npm start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
+
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Particles
+        style={{ position: "absolute" }}
+        height="100%"
+        width="100%"
+        params={{
+          particles: {
+            color: {
+              value: "#9b0000"
+            },
+            line_linked: {
+              color: {
+                value: "#c5050c"
+              }
+            },
+            number: {
+              value: 50
+            },
+            size: {
+              value: 3
+            }
+          }
+        }}
+      />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <code style={{color:"black"}}>BadgerHacks</code>
+        </header>
+      </div>
     </div>
   );
 }
