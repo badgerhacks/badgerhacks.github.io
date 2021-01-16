ec4723153de395650bb9e351e4ff8ef1e938b698
rdeepak2002 <rdeepak2002@gmail.com>
2021-01-16 14:24:21 -0800

    updated code

--- src/App.js
+++ src/App.js
@@ -310,8 +310,9 @@ class App extends Component {
             <li>
             <p>Participants can make anything they want.
             Prizes will be given to the most creative and unique projects.
-            The only restriction is that projects must be created during the
-            Hackathon (between January 21th to January 23rd).</p>
+            The only restriction is that <strong>projects must be created
+            during the Hackathon (between January 21st to
+             January 23rd).</strong></p>
             </li>
             <li>
             <p>Participants may work alone or in teams of up to 4 people.
@@ -327,6 +328,11 @@ class App extends Component {
             <p>This hackathon is open to everyone ages 18 and older,
             regardless of whether you are in College, and regardless of
             what College you attend.</p>
+            </li>
+            <li>
+            <p><strong>Any submission violating the UW Madison Code of Conduct or
+            any submission deemed as inappropriate by our team will
+            be removed.</strong></p>
             </li>
           </ul>
         </Container>
