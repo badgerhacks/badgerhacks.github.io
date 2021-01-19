2da5f15fc765e914aa4bc904dfdd5ed49fc818cf
rdeepak2002 <rdeepak2002@gmail.com>
2021-01-19 13:01:48 -0800

    updated code

--- src/App.js
+++ src/App.js
@@ -142,7 +142,8 @@ class App extends Component {
           <h1>About</h1>
           <p>Badger Hacks is a hackathon that takes place from
           January 21st to January 23rd (2021). It is run entirely by students at
-          the University of Wisconsin - Madison, and it is completely virtual.
+          the University of Wisconsin - Madison (UW-Madison), and it is
+          completely virtual.
           This means that regardless of where you are, as long as you have a
           submission that abides by our rules, you will be eligible for a
           prize.</p>
