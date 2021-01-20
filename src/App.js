716724707fa322a4a46a2c666c50eee063343ebb
rdeepak2002 <rdeepak2002@gmail.com>
2021-01-20 14:37:14 -0800

    updated code

--- src/App.js
+++ src/App.js
@@ -77,6 +77,11 @@ class App extends Component {
                 spy={true} smooth={true}>
                     Sponsors
                 </Link>
+
+                <Link className="nav-link" to="hall_of_fame"
+                activeClass="active" spy={true} smooth={true}>
+                    Hall of Fame
+                </Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
@@ -437,6 +442,195 @@ class App extends Component {
                 <a href="https://understoryweather.com/">
                   <Image src={understory_logo} alt="understory" fluid/>
                 </a>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+
+        {/* Hall of Fame Section */}
+        <Container id="hall_of_fame">
+          <h1>Hall of Fame</h1>
+          <Container>
+            <Row>
+              <Col align="center">
+                <h2 className="hof_year"
+                href="https://badgerhacks.devpost.com/project-gallery">
+                  Spring 2021
+                </h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col align="center">
+                <Card border="warning" style={{ width: '75%' }}>
+                  <Card.Header>
+                    <h3>1st</h3>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title>TBA</Card.Title>
+                    <Card.Text>
+                    {
+                      /*
+                      <iframe title="first_place" width="100%" height="100%"
+                        src="https://www.youtube-nocookie.com/embed/zuOUN-VSI40"
+                        frameborder="0"
+                        allow={"accelerometer; autoplay; clipboard-write;"+
+                        "encrypted-media; gyroscope; picture-in-picture"}
+                        allowfullscreen>
+                      </iframe>
+                      */
+                    }
+                    </Card.Text>
+                    {
+                      /*
+                      <Button className="top-btn-l full-width"
+                      href="https://badgerhacks.devpost.com/project-gallery">
+                        View Project
+                      </Button>
+                      */
+                    }
+                  </Card.Body>
+                </Card>
+                <Image/>
+              </Col>
+            </Row>
+            <Row>
+              <Col align="center">
+                <Card border="primary" style={{ width: '100%' }}>
+                  <Card.Header>
+                    <h3>2nd</h3>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title>TBA</Card.Title>
+                    <Card.Text>
+                    {
+                      /*
+                      <iframe title="first_place" width="100%" height="100%"
+                        src="https://www.youtube-nocookie.com/embed/zuOUN-VSI40"
+                        frameborder="0"
+                        allow={"accelerometer; autoplay; clipboard-write;"+
+                        "encrypted-media; gyroscope; picture-in-picture"}
+                        allowfullscreen>
+                      </iframe>
+                      */
+                    }
+                    </Card.Text>
+                    {
+                      /*
+                      <Button className="top-btn-l full-width"
+                      href="https://badgerhacks.devpost.com/project-gallery">
+                        View Project
+                      </Button>
+                      */
+                    }
+                  </Card.Body>
+                </Card>
+                <Image/>
+              </Col>
+              <Col align="center">
+                <Card border="primary" style={{ width: '100%' }}>
+                  <Card.Header>
+                    <h3>3rd</h3>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title>TBA</Card.Title>
+                    <Card.Text>
+                    {
+                      /*
+                      <iframe title="first_place" width="100%" height="100%"
+                        src="https://www.youtube-nocookie.com/embed/zuOUN-VSI40"
+                        frameborder="0"
+                        allow={"accelerometer; autoplay; clipboard-write;"+
+                        "encrypted-media; gyroscope; picture-in-picture"}
+                        allowfullscreen>
+                      </iframe>
+                      */
+                    }
+                    </Card.Text>
+                    {
+                      /*
+                      <Button className="top-btn-l full-width"
+                      href="https://badgerhacks.devpost.com/project-gallery">
+                        View Project
+                      </Button>
+                      */
+                    }
+                  </Card.Body>
+                </Card>
+                <Image/>
+              </Col>
+            </Row>
+            <Row>
+              <Col align="center">
+                <Card border="secondary" style={{ width: '100%' }}>
+                  <Card.Header>
+                    <h3>4th</h3>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title>TBA</Card.Title>
+                    <Card.Text>
+                    {
+                      /*
+                      <iframe title="first_place" width="100%" height="100%"
+                        src="https://www.youtube-nocookie.com/embed/zuOUN-VSI40"
+                        frameborder="0"
+                        allow={"accelerometer; autoplay; clipboard-write;"+
+                        "encrypted-media; gyroscope; picture-in-picture"}
+                        allowfullscreen>
+                      </iframe>
+                      */
+                    }
+                    </Card.Text>
+                    {
+                      /*
+                      <Button className="top-btn-l full-width"
+                      href="https://badgerhacks.devpost.com/project-gallery">
+                        View Project
+                      </Button>
+                      */
+                    }
+                  </Card.Body>
+                </Card>
+                <Image/>
+              </Col>
+              <Col align="center">
+                <Card border="secondary" style={{ width: '100%'}}>
+                  <Card.Header>
+                    <h3>5th</h3>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title>TBA</Card.Title>
+                    <Card.Text>
+                    {
+                      /*
+                      <iframe title="first_place" width="100%" height="100%"
+                        src="https://www.youtube-nocookie.com/embed/zuOUN-VSI40"
+                        frameborder="0"
+                        allow={"accelerometer; autoplay; clipboard-write;"+
+                        "encrypted-media; gyroscope; picture-in-picture"}
+                        allowfullscreen>
+                      </iframe>
+                      */
+                    }
+                    </Card.Text>
+                    {
+                      /*
+                      <Button className="top-btn-l full-width"
+                      href="https://badgerhacks.devpost.com/project-gallery">
+                        View Project
+                      </Button>
+                      */
+                    }
+                  </Card.Body>
+                </Card>
+                <Image/>
+              </Col>
+            </Row>
+            <Row>
+              <Col align="center">
+                <Button style={{marginBottom:"1rem"}} className="top-btn-r"
+                href="https://badgerhacks.devpost.com/project-gallery">
+                  View All Projects
+                </Button>
               </Col>
             </Row>
           </Container>
