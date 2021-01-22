fdfd463f8489455f5a2dd07325451b08a188d870
rdeepak2002 <rdeepak2002@gmail.com>
2021-01-22 15:35:32 -0800

    updated code

--- src/components/team.component.js
+++ src/components/team.component.js
@@ -18,64 +18,80 @@ class Team extends Component {
           {/* Founders Row */}
           <Row>
             {/* Deepak */}
-            <Col style={{ marginTop: "2rem" }} className="profile_col" align="center">
+            <Col style={{ marginTop: "2rem" }} className="profile_col"
+            align="center">
               <Card style={{ height: "100%" }}>
                 <Card.Header>
                   <strong>Co-Founder</strong>
                 </Card.Header>
                 <Row>
                   <Col align="center">
-                    <Image className="profile-image" src={deepak_profile} roundedCircle />
+                    <Image className="profile-image" src={deepak_profile}
+                    roundedCircle />
                   </Col>
                 </Row>
                 <Card.Body>
                   <Card.Title>Deepak Ramalingam</Card.Title>
                   <Card.Text>
-                    Deepak Ramalingam is a software developer at a PeeblesTech LLC, a local startup company in Madison, and an incoming augmented
-                    reality engineering intern at TikTok (US). He is a full-stack developer and utilizes his Computer Science knowledge to solve real
-                    world problems in the fields of education, business, health care, and emergency preparedness.
+                    Deepak Ramalingam is a software developer at a
+                    PeeblesTech LLC, a local startup company in Madison,
+                    and an incoming augmented reality engineering intern at
+                    TikTok (US). He is a full-stack developer and utilizes his
+                    Computer Science knowledge to solve real world problems
+                    in the fields of education, business, health care, and
+                    emergency preparedness.
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
             {/* Pranav */}
-            <Col style={{ marginTop: "2rem" }} className="profile_col" align="center">
+            <Col style={{ marginTop: "2rem" }} className="profile_col"
+            align="center">
               <Card style={{ height: "100%" }}>
                 <Card.Header>
                   <strong>Co-Founder</strong>
                 </Card.Header>
                 <Row>
                   <Col align="center">
-                    <Image className="profile-image" src={pranav_profile} roundedCircle />
+                    <Image className="profile-image" src={pranav_profile}
+                    roundedCircle />
                   </Col>
                 </Row>
                 <Card.Body>
                   <Card.Title>Pranav Kumar</Card.Title>
                   <Card.Text>
-                    Pranav Kumar is a freshman at the University of Wisconsin-Madison. Pranav is an avid coder, proficient in JAVA, C++, and Python.
-                    He enjoys Android development and following new advances in CS. Outside of Computer Science, Pranav plays badminton both
-                    recreationally and competitively and enjoys trying new foods.
+                    Pranav Kumar is a freshman at the University of
+                    Wisconsin-Madison. Pranav is an avid coder, proficient
+                    in JAVA, C++, and Python. He enjoys Android development
+                    and following new advances in CS. Outside of Computer
+                    Science, Pranav plays badminton both recreationally and
+                    competitively and enjoys trying new foods.
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
             {/* Stanley */}
-            <Col style={{ marginTop: "2rem" }} className="profile_col" align="center">
+            <Col style={{ marginTop: "2rem" }} className="profile_col"
+            align="center">
               <Card style={{ height: "100%" }}>
                 <Card.Header>
                   <strong>Co-Founder</strong>
                 </Card.Header>
                 <Row>
                   <Col align="center">
-                    <Image className="profile-image" src={stanley_profile} roundedCircle />
+                    <Image className="profile-image" src={stanley_profile}
+                    roundedCircle />
                   </Col>
                 </Row>
                 <Card.Body>
                   <Card.Title>Pranav Kumar</Card.Title>
                   <Card.Text>
-                    Stanley is a freshman at the University of Wisconsin-Madison. Stanley is fluent in java and python. He is an aspiring software
-                    engineer and has dabbled with data science and mobile app development. Some of his other interests are biking, fishing, and
-                    basketball.
+                    Stanley is a freshman at the University of
+                    Wisconsin-Madison. Stanley is fluent in java and
+                    python. He is an aspiring software engineer and has
+                    dabbled with data science and mobile app
+                    development. Some of his other interests are biking,
+                    fishing, and basketball.
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -85,14 +101,16 @@ class Team extends Component {
           {/*Other Row*/}
           <Row>
             {/* Dhananjayan */}
-            <Col style={{ marginTop: "2rem" }} className="profile_col" align="center">
+            <Col style={{ marginTop: "2rem" }} className="profile_col"
+            align="center">
               <Card style={{ height: "100%" }}>
                 <Card.Header>
                   <strong>Mentor & Judge</strong>
                 </Card.Header>
                 <Row>
                   <Col align="center">
-                    <Image className="profile-image" src={dhananjayan_profile} roundedCircle />
+                    <Image className="profile-image" src={dhananjayan_profile}
+                    roundedCircle />
                   </Col>
                 </Row>
                 <Card.Body>
@@ -111,36 +129,42 @@ class Team extends Component {
               </Card>
             </Col>
             {/* Eric */}
-            <Col style={{ marginTop: "2rem" }} className="profile_col" align="center">
+            <Col style={{ marginTop: "2rem" }} className="profile_col"
+            align="center">
               <Card style={{ height: "100%" }}>
                 <Card.Header>
                   <strong>Guest Judge</strong>
                 </Card.Header>
                 <Row>
                   <Col align="center">
-                    <Image className="profile-image" src={eric_profile} roundedCircle />
+                    <Image className="profile-image" src={eric_profile}
+                    roundedCircle />
                   </Col>
                 </Row>
                 <Card.Body>
                   <Card.Title>Eric Hewitt</Card.Title>
-                  <Card.Text>Eric Hewitt is the Chief Technology Officer at Understory, Inc.</Card.Text>
+                  <Card.Text>Eric Hewitt is the Chief Technology Officer
+                  at Understory, Inc.</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
             {/* Scott */}
-            <Col style={{ marginTop: "2rem" }} className="profile_col" align="center">
+            <Col style={{ marginTop: "2rem" }} className="profile_col"
+            align="center">
               <Card style={{ height: "100%" }}>
                 <Card.Header>
                   <strong>Guest Judge</strong>
                 </Card.Header>
                 <Row>
                   <Col align="center">
-                    <Image className="profile-image" src={scott_profile} roundedCircle />
+                    <Image className="profile-image" src={scott_profile}
+                    roundedCircle />
                   </Col>
                 </Row>
                 <Card.Body>
                   <Card.Title>Scott Rose</Card.Title>
-                  <Card.Text>Scott Rose is a Principal Engineer at Fetch Rewards, Inc.</Card.Text>
+                  <Card.Text>Scott Rose is a Principal Engineer at Fetch
+                  Rewards, Inc.</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
